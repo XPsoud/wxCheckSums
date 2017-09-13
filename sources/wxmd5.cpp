44ebe18c61a85d4dd968fde28d8be66fef5a4e1f
Xaviou <x.psoud@gmail.com>
2017-09-13 18:59:17 +0200

    Use gnu coreutils c files for MD5 (faster)

--- sources/wxmd5.cpp
+++ sources/wxmd5.cpp
@@ -1,59 +1,4 @@
-/// About the MD5 algorithm used here :
-///     Based on md5.h and md5.c reference implemantion of RFC 1321
-///     Derived from the RSA Data Security, Inc.
-///     MD5 Message-Digest Algorithm
-///     Copyright (C) 1991 - 1992 RSA Data Security, Inc. Created 1991.
-///     All rights reserved.
-
 #include "wxmd5.h"
-
-// Constants for MD5Transform routine.
-#define S11 7
-#define S12 12
-#define S13 17
-#define S14 22
-#define S21 5
-#define S22 9
-#define S23 14
-#define S24 20
-#define S31 4
-#define S32 11
-#define S33 16
-#define S34 23
-#define S41 6
-#define S42 10
-#define S43 15
-#define S44 21
-
-// Basic MD5 functions.
-inline uint32_t wxMD5::F(uint32_t x, uint32_t y, uint32_t z) { return (x&y) | (~x&z); }
-
-inline uint32_t wxMD5::G(uint32_t x, uint32_t y, uint32_t z) { return (x&z) | (y&~z); }
-
-inline uint32_t wxMD5::H(uint32_t x, uint32_t y, uint32_t z) { return x^y^z; }
-
-inline uint32_t wxMD5::I(uint32_t x, uint32_t y, uint32_t z) { return y ^ (x | ~z); }
-
-// Rotate x left n bits.
-inline uint32_t wxMD5::rotate_left(uint32_t x, int n) { return (x << n) | (x >> (32-n)); }
-
-// FF, GG, HH, and II transformations for rounds 1, 2, 3, and 4.
-// Rotation is separate from addition to prevent recomputation.
-inline void wxMD5::FF(uint32_t &a, uint32_t b, uint32_t c, uint32_t d, uint32_t x, uint32_t s, uint32_t ac) {
-	a = rotate_left(a+ F(b,c,d) + x + ac, s) + b;
-}
-
-inline void wxMD5::GG(uint32_t &a, uint32_t b, uint32_t c, uint32_t d, uint32_t x, uint32_t s, uint32_t ac) {
-	a = rotate_left(a + G(b,c,d) + x + ac, s) + b;
-}
-
-inline void wxMD5::HH(uint32_t &a, uint32_t b, uint32_t c, uint32_t d, uint32_t x, uint32_t s, uint32_t ac) {
-	a = rotate_left(a + H(b,c,d) + x + ac, s) + b;
-}
-
-inline void wxMD5::II(uint32_t &a, uint32_t b, uint32_t c, uint32_t d, uint32_t x, uint32_t s, uint32_t ac) {
-	a = rotate_left(a + I(b,c,d) + x + ac, s) + b;
-}
 
 wxMD5::wxMD5(const wxString& text)
 {
@@ -77,193 +22,23 @@ wxMD5::~wxMD5()
 
 void wxMD5::Initialize()
 {
+	md5_init_ctx(&m_context);
+
 	m_bFinalized=false;
-
-	m_uiCount[0] = 0;
-	m_uiCount[1] = 0;
-
-	// Load magic initialization constants.
-	m_uiState[0] = 0x67452301;
-	m_uiState[1] = 0xefcdab89;
-	m_uiState[2] = 0x98badcfe;
-	m_uiState[3] = 0x10325476;
-}
-
-// Decode input (8 bits) into output (32 bits). Assumes len is a multiple of 4.
-void wxMD5::Decode(uint32_t output[], const uint8_t input[], uint32_t len)
-{
-	for (unsigned int i = 0, j = 0; j < len; i++, j += 4)
-		output[i] = ((uint32_t)input[j]) | (((uint32_t)input[j+1]) << 8) |
-			(((uint32_t)input[j+2]) << 16) | (((uint32_t)input[j+3]) << 24);
-}
-
-// Encode input (32 bits) into output (8 bits). Assumes len is a multiple of 4.
-void wxMD5::Encode(uint8_t output[], const uint32_t input[], uint32_t len)
-{
-	for (uint32_t i = 0, j = 0; j < len; i++, j += 4) {
-		output[j] = input[i] & 0xff;
-		output[j+1] = (input[i] >> 8) & 0xff;
-		output[j+2] = (input[i] >> 16) & 0xff;
-		output[j+3] = (input[i] >> 24) & 0xff;
-	}
-}
-
-// Apply MD5 algorithm on a block
-void wxMD5::Transform(const uint8_t block[64])
-{
-	uint32_t a = m_uiState[0], b = m_uiState[1], c = m_uiState[2], d = m_uiState[3], x[16];
-	Decode (x, block, 64);
-
-	/* Round 1 */
-	FF (a, b, c, d, x[ 0], S11, 0xd76aa478); /* 1 */
-	FF (d, a, b, c, x[ 1], S12, 0xe8c7b756); /* 2 */
-	FF (c, d, a, b, x[ 2], S13, 0x242070db); /* 3 */
-	FF (b, c, d, a, x[ 3], S14, 0xc1bdceee); /* 4 */
-	FF (a, b, c, d, x[ 4], S11, 0xf57c0faf); /* 5 */
-	FF (d, a, b, c, x[ 5], S12, 0x4787c62a); /* 6 */
-	FF (c, d, a, b, x[ 6], S13, 0xa8304613); /* 7 */
-	FF (b, c, d, a, x[ 7], S14, 0xfd469501); /* 8 */
-	FF (a, b, c, d, x[ 8], S11, 0x698098d8); /* 9 */
-	FF (d, a, b, c, x[ 9], S12, 0x8b44f7af); /* 10 */
-	FF (c, d, a, b, x[10], S13, 0xffff5bb1); /* 11 */
-	FF (b, c, d, a, x[11], S14, 0x895cd7be); /* 12 */
-	FF (a, b, c, d, x[12], S11, 0x6b901122); /* 13 */
-	FF (d, a, b, c, x[13], S12, 0xfd987193); /* 14 */
-	FF (c, d, a, b, x[14], S13, 0xa679438e); /* 15 */
-	FF (b, c, d, a, x[15], S14, 0x49b40821); /* 16 */
-
-	/* Round 2 */
-	GG (a, b, c, d, x[ 1], S21, 0xf61e2562); /* 17 */
-	GG (d, a, b, c, x[ 6], S22, 0xc040b340); /* 18 */
-	GG (c, d, a, b, x[11], S23, 0x265e5a51); /* 19 */
-	GG (b, c, d, a, x[ 0], S24, 0xe9b6c7aa); /* 20 */
-	GG (a, b, c, d, x[ 5], S21, 0xd62f105d); /* 21 */
-	GG (d, a, b, c, x[10], S22,  0x2441453); /* 22 */
-	GG (c, d, a, b, x[15], S23, 0xd8a1e681); /* 23 */
-	GG (b, c, d, a, x[ 4], S24, 0xe7d3fbc8); /* 24 */
-	GG (a, b, c, d, x[ 9], S21, 0x21e1cde6); /* 25 */
-	GG (d, a, b, c, x[14], S22, 0xc33707d6); /* 26 */
-	GG (c, d, a, b, x[ 3], S23, 0xf4d50d87); /* 27 */
-	GG (b, c, d, a, x[ 8], S24, 0x455a14ed); /* 28 */
-	GG (a, b, c, d, x[13], S21, 0xa9e3e905); /* 29 */
-	GG (d, a, b, c, x[ 2], S22, 0xfcefa3f8); /* 30 */
-	GG (c, d, a, b, x[ 7], S23, 0x676f02d9); /* 31 */
-	GG (b, c, d, a, x[12], S24, 0x8d2a4c8a); /* 32 */
-
-	/* Round 3 */
-	HH (a, b, c, d, x[ 5], S31, 0xfffa3942); /* 33 */
-	HH (d, a, b, c, x[ 8], S32, 0x8771f681); /* 34 */
-	HH (c, d, a, b, x[11], S33, 0x6d9d6122); /* 35 */
-	HH (b, c, d, a, x[14], S34, 0xfde5380c); /* 36 */
-	HH (a, b, c, d, x[ 1], S31, 0xa4beea44); /* 37 */
-	HH (d, a, b, c, x[ 4], S32, 0x4bdecfa9); /* 38 */
-	HH (c, d, a, b, x[ 7], S33, 0xf6bb4b60); /* 39 */
-	HH (b, c, d, a, x[10], S34, 0xbebfbc70); /* 40 */
-	HH (a, b, c, d, x[13], S31, 0x289b7ec6); /* 41 */
-	HH (d, a, b, c, x[ 0], S32, 0xeaa127fa); /* 42 */
-	HH (c, d, a, b, x[ 3], S33, 0xd4ef3085); /* 43 */
-	HH (b, c, d, a, x[ 6], S34,  0x4881d05); /* 44 */
-	HH (a, b, c, d, x[ 9], S31, 0xd9d4d039); /* 45 */
-	HH (d, a, b, c, x[12], S32, 0xe6db99e5); /* 46 */
-	HH (c, d, a, b, x[15], S33, 0x1fa27cf8); /* 47 */
-	HH (b, c, d, a, x[ 2], S34, 0xc4ac5665); /* 48 */
-
-	/* Round 4 */
-	II (a, b, c, d, x[ 0], S41, 0xf4292244); /* 49 */
-	II (d, a, b, c, x[ 7], S42, 0x432aff97); /* 50 */
-	II (c, d, a, b, x[14], S43, 0xab9423a7); /* 51 */
-	II (b, c, d, a, x[ 5], S44, 0xfc93a039); /* 52 */
-	II (a, b, c, d, x[12], S41, 0x655b59c3); /* 53 */
-	II (d, a, b, c, x[ 3], S42, 0x8f0ccc92); /* 54 */
-	II (c, d, a, b, x[10], S43, 0xffeff47d); /* 55 */
-	II (b, c, d, a, x[ 1], S44, 0x85845dd1); /* 56 */
-	II (a, b, c, d, x[ 8], S41, 0x6fa87e4f); /* 57 */
-	II (d, a, b, c, x[15], S42, 0xfe2ce6e0); /* 58 */
-	II (c, d, a, b, x[ 6], S43, 0xa3014314); /* 59 */
-	II (b, c, d, a, x[13], S44, 0x4e0811a1); /* 60 */
-	II (a, b, c, d, x[ 4], S41, 0xf7537e82); /* 61 */
-	II (d, a, b, c, x[11], S42, 0xbd3af235); /* 62 */
-	II (c, d, a, b, x[ 2], S43, 0x2ad7d2bb); /* 63 */
-	II (b, c, d, a, x[ 9], S44, 0xeb86d391); /* 64 */
-
-	m_uiState[0] += a;
-	m_uiState[1] += b;
-	m_uiState[2] += c;
-	m_uiState[3] += d;
-
-	// Zeroize sensitive information.
-	memset(x, 0, sizeof x);
 }
 
 // Continue an MD5 message-digest operation, processing another message block
-void wxMD5::Update(const unsigned char input[], uint32_t length)
+void wxMD5::Update(const unsigned char *message, unsigned int len)
 {
-	// Compute number of bytes mod 64
-	uint32_t index = m_uiCount[0] / 8 % 64;
-
-	// Update number of bits
-	if ((m_uiCount[0] += (length << 3)) < (length << 3))
-		m_uiCount[1]++;
-	m_uiCount[1] += (length >> 29);
-
-	// Number of bytes we need to fill in buffer
-	uint32_t firstpart = 64 - index;
-
-	uint32_t i;
-
-	// Transform as many times as possible.
-	if (length >= firstpart)
-	{
-		// Fill buffer first, transform
-		memcpy(&m_uiBuffer[index], input, firstpart);
-		Transform(m_uiBuffer);
-
-		// Transform chunks of blocksize (64 bytes)
-		for (i = firstpart; i + 64 <= length; i += 64)
-			Transform(&input[i]);
-
-		index = 0;
-	}
-	else
-		i = 0;
-
-	// Buffer remaining input
-	memcpy(&m_uiBuffer[index], &input[i], length-i);
+	md5_process_bytes(message, len, &m_context);
 }
 
 // End an MD5 message-digest operation, writing the digest and zeroizing the context.
-wxMD5& wxMD5::Finalize()
+void wxMD5::Finalize()
 {
-	static unsigned char padding[64] = {
-		0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-	};
+	md5_finish_ctx(&m_context, m_uiDigest);
 
-	if (!m_bFinalized) {
-		// Save number of bits
-		unsigned char bits[8];
-		Encode(bits, m_uiCount, 8);
-
-		// pad out to 56 mod 64.
-		uint32_t index = m_uiCount[0] / 8 % 64;
-		uint32_t padLen = (index < 56) ? (56 - index) : (120 - index);
-		Update(padding, padLen);
-
-		// Append length (before padding)
-		Update(bits, 8);
-
-		// Store state in digest
-		Encode(m_uiDigest, m_uiState, 16);
-
-		// Zeroize sensitive information.
-		memset(m_uiBuffer, 0, 64);
-		memset(m_uiCount, 0, 2);
-
-		m_bFinalized=true;
-	}
-
-	return *this;
+	m_bFinalized=true;
 }
 
 wxString wxMD5::HexDigest() const
@@ -272,7 +47,7 @@ wxString wxMD5::HexDigest() const
 		return wxEmptyString;
 
 	wxString sResult=wxEmptyString;
-	for (int i=0; i<16; i++)
+	for (int i=0; i<MD5_DIGEST_SIZE; i++)
 		sResult << wxString::Format(_T("%02x"), m_uiDigest[i]);
 
 	return sResult;
--- sources/wxmd5.h
+++ sources/wxmd5.h
@@ -1,47 +1,23 @@
-/// About the MD5 algorithm used here :
-///     Based on md5.h and md5.c reference implemantion of RFC 1321
-///     Derived from the RSA Data Security, Inc.
-///     MD5 Message-Digest Algorithm
-///     Copyright (C) 1991 - 1992 RSA Data Security, Inc. Created 1991.
-///     All rights reserved.
-
 #ifndef __WXMD5_H_INCLUDED__
 #define __WXMD5_H_INCLUDED__
 
-#include <stdint.h>
-
 #include <wx/wx.h>
+
+#include "md5.h"
 
 class wxMD5
 {
 	public:
 		wxMD5(const wxString& text=wxEmptyString);
-		virtual ~wxMD5();
-		void Update(const unsigned char *buf, uint32_t length);
-		wxMD5& Finalize();
+		~wxMD5();
+		void Update(const unsigned char *message, unsigned int len);
+		void Finalize();
 		wxString HexDigest() const;
 	private:
 		void Initialize();
-		void Transform(const uint8_t block[64]);
-		static void Decode(uint32_t output[], const uint8_t input[], uint32_t len);
-		static void Encode(uint8_t output[], const uint32_t input[], uint32_t len);
-
 		bool m_bFinalized;
-		uint8_t m_uiBuffer[64];              // bytes that didn't fit in last 64 byte chunk
-		uint32_t m_uiCount[2];               // 64bit counter for number of bits (lo, hi)
-		uint32_t m_uiState[4];               // digest so far
-		uint8_t m_uiDigest[16];              // result
-
-		// low level logic operations
-		static inline uint32_t F(uint32_t x, uint32_t y, uint32_t z);
-		static inline uint32_t G(uint32_t x, uint32_t y, uint32_t z);
-		static inline uint32_t H(uint32_t x, uint32_t y, uint32_t z);
-		static inline uint32_t I(uint32_t x, uint32_t y, uint32_t z);
-		static inline uint32_t rotate_left(uint32_t x, int n);
-		static inline void FF(uint32_t &a, uint32_t b, uint32_t c, uint32_t d, uint32_t x, uint32_t s, uint32_t ac);
-		static inline void GG(uint32_t &a, uint32_t b, uint32_t c, uint32_t d, uint32_t x, uint32_t s, uint32_t ac);
-		static inline void HH(uint32_t &a, uint32_t b, uint32_t c, uint32_t d, uint32_t x, uint32_t s, uint32_t ac);
-		static inline void II(uint32_t &a, uint32_t b, uint32_t c, uint32_t d, uint32_t x, uint32_t s, uint32_t ac);
+		uint8_t m_uiDigest[MD5_DIGEST_SIZE];
+		md5_ctx m_context;
 };
 
 #endif // __WXMD5_H_INCLUDED__
