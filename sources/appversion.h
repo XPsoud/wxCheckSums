c4b39c629192e1bf09341fa0a9c08348f6e68d13
Xaviou <x.psoud@gmail.com>
2024-03-24 15:57:47 +0100

    Swtch to wx-3.3.x (CB) and wx-3.2.x (Makefile)

--- sources/appversion.h
+++ sources/appversion.h
@@ -4,15 +4,15 @@
 #define VERSION_MAJOR	0
 #define VERSION_MINOR	0
 #define VERSION_REV     1
-// Add "201" before the number below to obtain
+// Add "202" before the number below to obtain
 // the complete date in format : YYYYMMJJ
-#define VERSION_BUILD   70914
+#define VERSION_BUILD   40324
 
 #define PRODUCTNAME "wxCheckSums"
 #define DESCRIPTION "wxCheckSums Application"
 #define wDESCRIPTION _("wxCheckSums Application")
 
-#define COPYRIGHT "Copyright (c) X.P. 2017"
-#define SMALLCOPYRIGHT "(c) X.P. 2017"
+#define COPYRIGHT "Copyright (c) X.P. 2024"
+#define SMALLCOPYRIGHT "(c) X.P. 2024"
 
 #endif // __APPVERSION_H_INCLUDED__
